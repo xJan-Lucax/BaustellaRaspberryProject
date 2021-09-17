0a9d378d39376fc4b7a31a31d6f06a083c8eb3fa
xJan-Lucax <jan-luca.nettingsmeier@fh-bielefeld.de>
2021-09-17 13:29:42 +0200

    Waage Testprogramm

--- Waage.py
+++ Waage.py
@@ -9,9 +9,14 @@ try:
         gain=128
     )
 
+    print("1")
     hx711.reset()   # Before we start, reset the HX711 (not obligate)
+    print("2")
     measures = hx711.get_raw_data(num_measures=3)
+    print("3")
 finally:
+    print("4")
     GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
 
+print("5")
 print("\n".join(measures))
