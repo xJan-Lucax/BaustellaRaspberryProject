f534ea6d2ae39e58c72cd0aaf8a32130a7724943
xJan-Lucax <jan-luca.nettingsmeier@fh-bielefeld.de>
2021-09-17 11:39:59 +0200

    Waage Testprogramm

---
+++ Waage.py
@@ -0,0 +1,15 @@
+from snappy_hx711 import *
+
+CLOCK_PIN = 6
+DATA_PIN = 7
+
+@setHook(HOOK_INIT)
+def init():
+    # Must be called before making measurements:
+    hx711_begin(CLOCK_PIN, DATA_PIN)
+
+def take_a_measurement():
+    # Returns a 3-byte string:
+    return hx711_read()
+
+
