8798ee9334024b51edd7d2f08977b0d84805bb0e
xJan-Lucax <jan-luca.nettingsmeier@fh-bielefeld.de>
2021-09-17 12:51:03 +0200

    Waage Testprogramm

--- Waage.py
+++ Waage.py
@@ -4,6 +4,7 @@ CLOCK_PIN = 6
 DATA_PIN = 7
 
 @setHook(HOOK_INIT)
+
 def init():
     # Must be called before making measurements:
     hx711_begin(CLOCK_PIN, DATA_PIN)
@@ -12,4 +13,7 @@ def take_a_measurement():
     # Returns a 3-byte string:
     return hx711_read()
 
-
+init()
+while True:
+    take_a_measurement()
+    print("I DO")
